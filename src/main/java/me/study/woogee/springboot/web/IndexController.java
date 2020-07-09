a04a1c655190f3a2fe13c48262b45f1ee17b4910
WoogeeLee <gogojet86@gmail.com>
2020-07-09 17:29:48 +0900

    chapter05 End

--- src/main/java/me/study/woogee/springboot/web/IndexController.java
+++ src/main/java/me/study/woogee/springboot/web/IndexController.java
@@ -1,6 +1,8 @@
 package me.study.woogee.springboot.web;
 
 import lombok.RequiredArgsConstructor;
+import me.study.woogee.springboot.config.auth.LoginUser;
+import me.study.woogee.springboot.config.auth.dto.SessionUser;
 import me.study.woogee.springboot.service.PostsService;
 import me.study.woogee.springboot.web.dto.PostsResponseDto;
 import org.springframework.stereotype.Controller;
@@ -8,15 +10,23 @@ import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 
+import javax.servlet.http.HttpSession;
+
 @RequiredArgsConstructor
 @Controller
 public class IndexController {
 
     private final PostsService postsService;
+    private final HttpSession httpSession;
 
     @GetMapping("/")
-    public String index(Model model) {
+    public String index(Model model, @LoginUser SessionUser user) {
         model.addAttribute("posts", postsService.findAllDesc());
+
+        if(user != null) {
+            model.addAttribute("userName", user.getName());
+        }
+
         return "index";
     }
 
