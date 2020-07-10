f79e1e511256f84ce738792210a7e1ddeed640c3
WoogeeLee <gogojet86@gmail.com>
2020-07-10 18:04:38 +0900

    port가 잘 안가져와져서 다시 수정3 소괄호구나..

--- scripts/stop.sh
+++ scripts/stop.sh
@@ -4,7 +4,7 @@ ABSPATH=$(readlink -f $0)
 ABSDIR=$(dirname $ABSPATH)
 source ${ABSDIR}/profile.sh
 
-IDLE_PORT=${find_idle_port}
+IDLE_PORT=$(find_idle_port)
 
 echo "> $IDLE_PORT 에서 구동중인 application pid 확인"
 
