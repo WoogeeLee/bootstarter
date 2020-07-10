574499e6752b397f47e16c402c3ca60305f16fec
WoogeeLee <gogojet86@gmail.com>
2020-07-10 16:51:17 +0900

    파일이 안감 다시 수정2

--- scripts/stop.sh
+++ scripts/stop.sh
@@ -8,7 +8,7 @@ IDLE_PORT=${find_idle_port}
 
 echo "> $IDLE_PORT 에서 구동중인 application pid 확인"
 
-IDLE_PID=$(lsof -ti tcp:"${IDLE_PORT}")
+IDLE_PID=$(lsof -ti tcp:${IDLE_PORT})
 
 if [ -z "${IDLE_PID}" ]
 then
