b2482d3f04162ff9a49fad0053dae027f73c63a7
WoogeeLee <gogojet86@gmail.com>
2020-07-10 16:40:10 +0900

    파일이 안감 다시 수정

--- scripts/stop.sh
+++ scripts/stop.sh
@@ -8,9 +8,9 @@ IDLE_PORT=${find_idle_port}
 
 echo "> $IDLE_PORT 에서 구동중인 application pid 확인"
 
-IDLE_PID=${lsof -ti tcp:${IDLE_PORT}}
+IDLE_PID=$(lsof -ti tcp:"${IDLE_PORT}")
 
-if [ -z ${IDLE_PID} ]
+if [ -z "${IDLE_PID}" ]
 then
   echo "> 현재 구동중인 application이 없으므로 종료하지 않습니다."
 else
